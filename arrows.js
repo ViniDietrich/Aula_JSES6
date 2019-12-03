6f507d0ce30f2a33ccc1b88f52abb7643adb58c3
Vinicius <vin.dietrich@gmail.com>
2019-12-03 15:40:39 -0300

    commit arrowsa

--- arrows.js
+++ arrows.js
@@ -43,6 +43,110 @@ var logzera = value => {
     console.log(value);
 };*/
 
-var Obj = {
-    log: function
+/*
+var obj = {
+    showContext: function showContext() {
+        this.log('teste');
+
+        //this = obj
+
+        setTimeout(() => {
+            this.log('after 1000ms')
+        }, 1000);
+    },
+    log: function log(value) {
+       console.log(value);
+    }
+};
+
+obj.showContext();
+*/
+
+/*
+//lazy valuation
+function randomNumber(){
+    console.log('Gerando um numero aleatorio...');
+
+    return Math.random() * 10;
 }
+
+console.log(randomNumber());
+
+// definindo b, caso não tenha valor o b
+function multiply (a = 2, b = a, c = randomNumber()) {
+    return a * b * c;
+};
+
+console.log(multiply(5));
+*/
+
+// Forma clássica
+
+var prop1 = 'Digital Innovation One'
+
+var obj = {
+    prop1: prop1
+};
+ 
+console.log(obj)
+
+//Nova forma
+
+var prop1 = 'Digital Innovation One'
+
+var obj = {
+    prop1
+};
+ 
+console.log(obj)
+
+//também para funções
+
+function method1 (){
+    console.log('method called')
+}
+
+var obj = {
+    method1
+}
+obj.method1()
+
+//Outra forma
+
+var obj = {
+    sum: function sum(a,b) {
+        return a+b
+    }
+};
+
+console.log(obj.sum(1,5));
+
+// Nova forma
+
+var obj = {
+    sum1(a, b){
+        return a + b;
+    }
+};
+
+console.log(obj.sum1(1, 5));
+
+//Outro caso
+
+var propName = 'teste';
+
+var obj = {}
+
+obj[propName + ' novo'] = 'prop value'
+
+console.log(obj);
+
+// No modo
+
+var propName = 'teste';
+
+var obj = {
+    [propName + ' novo']: 'prop value'
+};
+
+console.log(obj);
