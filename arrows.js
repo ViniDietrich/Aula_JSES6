38e845fce2f9cdfe0452626e00690c1b91eb91ed
Vinicius <vin.dietrich@gmail.com>
2019-12-03 14:35:04 -0300

    commmit 03/12

---
+++ arrows.js
@@ -0,0 +1,48 @@
+/*
+var sumold = function(a, b){
+    return a + b;
+};
+
+console.log(sumold(5, 5));
+
+// Arrow Function
+
+var sum = (a,b) => {
+    return a+b;
+}
+
+console.log(sum(5, 15));
+
+// Construir Objeto
+var createObj = () => ({ teste: 123 });
+
+console.log(createObj())
+
+// Função construtora
+function Car() {
+    this.foo = 'bar';
+}
+
+console.log(new Car ());
+
+// arrow construtora - Não funciona com o this
+
+
+var Car1 = () => {
+    this.foo = 'bar';
+};
+
+console.log(new Car1());
+
+
+// Denominar a variavel antes
+
+logzera ('teste')
+
+var logzera = value => {
+    console.log(value);
+};*/
+
+var Obj = {
+    log: function
+}
---
+++ new.js
@@ -0,0 +1,9 @@
+function caloriasDeTrote(x) {
+  var calorias = 5;
+  for(var i=1;i<=x;i++) {
+    calorias = calorias * i; 
+  }
+  return calorias
+}
+
+caloriasDeTrote(2)
