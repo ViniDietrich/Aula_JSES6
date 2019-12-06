2382e51144d4998ef007f27d1a973a5bb0babdb2
Vinicius <vin.dietrich@gmail.com>
2019-12-06 16:20:49 -0300

    santander final

--- santander.js
+++ santander.js
@@ -39,3 +39,49 @@ function escada(tamanho) {
 }
 
 console.log(escada(5));
+
+function acontece(hrEntrada, minimo) {
+    var naHora = 0;
+    for(var i=0; i <= hrEntrada.length; i++){
+        if (hrEntrada[i] <= 0) {
+            naHora++;
+        } 
+    }
+    return naHora >= minimo;
+}
+
+function aberturas (filtro, minimo){
+    var naAula = [];
+    for (var i = 0; i < filtro.length; i++){
+    	naAula.push(acontece(filtro[i], minimo)); 
+    }
+    return naAula; 
+}
+
+var alunosDaSegunda = [10, -5, 3, 0];
+
+console.log(acontece(alunosDaSegunda, 2));
+
+function alturaArvoreUtopica(ciclos) {
+    var crescimento = 1;
+    for (var i=0; i < ciclos; i++){
+        if (i%2){
+            crescimento += 1;
+        } else {
+            crescimento *= 2;
+        }
+    }
+    return crescimento;
+}
+
+console.log(alturaArvoreUtopica(5))
+
+function fatorial (numero) {
+    var fatorado = 1;
+    for(var i=1; i <= numero; i++) {
+        fatorado = fatorado * i;
+    }
+    return fatorado;
+}
+
+console.log(fatorial(4));
